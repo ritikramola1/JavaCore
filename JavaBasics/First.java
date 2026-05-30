9ded2d805ab93a718f2e4f09970940794c14161a
ritikramola1 <ritik.ramola@globallogic.com>
2026-05-30 15:41:08 +0000

    basics1

---
+++ JavaBasics/First.java
@@ -0,0 +1,45 @@
+// // Java variables
+
+// class First{
+//     public static void main(String[] args) {
+        
+//         byte a=1;
+//         int rit = 29;
+
+//         float abc = 2.1f;  //we use f as ,double is default in java
+//         double ab = 2.5;
+
+//         long bcd = 1242343243;
+
+//         char cdc='s'; //  we use single quotes //Double is for String
+
+//         System.out.println(ab +" he " + abc  + " he " + a + " he " + rit + " he " + bcd + " he " + cdc);
+//     }
+
+// }
+
+// //--------------------------------- // Casting in Java
+
+// class First{
+//     public static void main(String[] args) {
+        
+//         int a=1;
+//         double b = a; // implicit casting=internal lower to higher data type
+
+//         System.out.println(b);
+
+//         double c = 2.5;
+//         int d = (int) c; // explicit casting = external higher to lower data type we have to use (int) before the variable name
+
+//         System.out.println(d);
+
+//         //this is called type promotion in java , as input is of lower data type and output is of higher data type
+//         byte f = 10;
+//         byte g = 20;
+
+//         int res = f*g; // byte and byte gives int as output
+//         System.out.println(res);
+//     }
+
+// }
+
