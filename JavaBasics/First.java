4e5bdfe2ab8ed236f6d3e2003b9d160172536d88
ritikramola1 <ritik.ramola@globallogic.com>
2026-06-02 15:14:13 +0000

    operations

--- JavaBasics/First.java
+++ JavaBasics/First.java
@@ -43,3 +43,100 @@
 
 // }
 
+// --------------------------------- // Operators in Java
+// == , !=, >, <, >=, <= are called comparison operators
+// &&, ||, ! are called logical operators
+// ++, -- are called increment and decrement operators
+
+//if else statement in java
+// class First{
+//     public static void main(String[] args) {
+        
+//         int age = 18;
+
+//         if(age>=18){
+//             System.out.println("You are eligible to vote");
+//         }
+//         else{
+//             System.out.println("You are not eligible to vote");
+//         }
+//     }
+
+// }
+
+//ternary operator in java
+//it is a shorthand for if else statement used by ? and :
+// ? is used for if and : is used for else
+
+// class First{
+//     public static void main(String[] args) {        
+//         int age = 18;
+//         String res = (age>=18) ? "You are eligible to vote" : "You are not eligible to vote";
+//         System.out.println(res);
+//     }
+// }
+
+// -------------------- // switch case in java ---------------
+// class First{
+//     public static void main(String[] args) {        
+//         int day = 3;
+//         switch(day){
+//             case 1:
+//                 System.out.println("Monday");
+//                 break;
+//             case 2:
+//                 System.out.println("Tuesday");
+//                 break;
+//             case 3:
+//                 System.out.println("Wednesday");
+//                 break;
+//             case 4:
+//                 System.out.println("Thursday");
+//                 break;
+//             case 5:
+//                 System.out.println("Friday");
+//                 break;
+//             case 6:
+//                 System.out.println("Saturday");
+//                 break;
+//             case 7:
+//                 System.out.println("Sunday");
+//                 break;
+//             default:
+//                 System.out.println("Invalid day");
+//         }
+//     }
+// }
+
+// -------------------- // Loops in Java ---------------
+// for loop in java
+
+// class First{
+//     public static void main(String[] args) {        
+//         for(int i=0; i<5; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// -------------------- // while loop in java
+// class First{
+//     public static void main(String[] args) {        
+//         int i=0;
+//         while(i<5){
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+// -------------------- // do while loop in java
+// class First{
+//     public static void main(String[] args) {        
+//         int i=0;
+//         do{
+//             System.out.println(i);
+//             i++;
+//         }while(i<5);
+//     }
+// }
